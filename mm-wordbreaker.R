74f340040edcae41e66268870c23dee9a09c96e3
Alexander Beatson <hello@alexbeatson.me>
2024-03-12 16:52:57 +0700

    V2-fix issues and add features
    Add language out-of-scope detection & space-splitting
    Remove non-burmese character sequencing. 
    Remove unnecessary block
    Now works either ့ + ် or ် + ့ properly

--- mm-wordbreaker.R
+++ mm-wordbreaker.R
@@ -3,6 +3,6 @@ mmwb <- function (x) {
   if(!is.vector(x)) {
     stop("Your input is not vector.")
   } else {
-    return(str_extract_all(x, "([\U1000-\U102A][!^\U102B-\U103E]{0,8}[\U1000-\U1027][\U103A|\U1039][!^\U102B-\U103E\\s]{0,5})|([\U1000-\U102A][!^\U102B-\U103E]{0,8})|([\U1040-\U1049]+)|([\U103F\U104C-\U104F])|([^\U1000-\U104F\\s]+)"))
+    return(str_extract_all(x, "([\U1000-\U102A][\U102B-\U103E]{0,8}[\U1000-\U1027][\u1037]?[\U103A|\U1039][\U102B-\U103E\\s]{0,5})|([\U1000-\U102A][\U102B-\U103E]{0,8})|([\U1040-\U1049]+)|([\U103F\U104C-\U104F])|([\u21-\uFFF]+)"))
   }
 }
